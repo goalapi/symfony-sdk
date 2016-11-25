9b286fc17f612e31a8a433a480971914f1228cb7
Murat Erkenov <murat@11bits.net>
2016-11-25 14:59:55 +0300

    Bundle Class Added

---
+++ GoalAPISDKBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 11/25/16/11:29 AM
+ *
+ */
+
+namespace GoalAPI\SDKBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GoalAPISDKBundle extends Bundle
+{
+
+}
