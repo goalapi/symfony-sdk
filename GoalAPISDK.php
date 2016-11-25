9c926b0aa87a8246b370fb1ae31e3117967c717b
Murat Erkenov <murat@11bits.net>
2016-11-25 15:47:18 +0300

    Base SDK class added to core

--- GoalAPISDK.php
+++ GoalAPISDK.php
@@ -7,54 +7,7 @@
 
 namespace GoalAPI\SDKBundle;
 
-class GoalAPISDK
+class GoalAPISDK extends Core\SDK
 {
-    /**
-     * @var Core\CallPerformerInterface[]
-     */
-    private $callPerformers;
-
-
-    function __call($name, $arguments)
-    {
-        return $this->makeCall($name, $arguments);
-    }
-
-    /**
-     * @param $name
-     * @param $arguments
-     * @return mixed
-     */
-    public function makeCall($name, $arguments)
-    {
-        if (!isset($this->callPerformers[$name])) {
-            throw new \BadMethodCallException();
-        }
-        /** @var Core\CallPerformerInterface $callPerformer */
-        $callPerformer = $this->callPerformers[$name];
-
-        return $callPerformer->performCall($arguments);
-    }
-
-    /**
-     * @param Core\CallPerformerInterface[] $callPerformers
-     */
-    public function setCallPerformers(array $callPerformers)
-    {
-        $this->callPerformers = [];
-        /** @var Core\CallPerformerInterface $performer */
-        foreach ($callPerformers as $callName => $performer) {
-            $this->addCallPerformer($callName, $performer);
-        }
-    }
-
-    /**
-     * @param string $callName
-     * @param Core\CallPerformerInterface $callPerformer
-     */
-    public function addCallPerformer($callName, Core\CallPerformerInterface $callPerformer)
-    {
-        $this->callPerformers[$callName] = $callPerformer;
-    }
 
 }
