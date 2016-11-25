db79a70e43ef8bf69e2b290edd622ba7816c566f
Murat Erkenov <murat@11bits.net>
2016-11-25 15:05:27 +0300

    GoalAPISDK class added

---
+++ GoalAPISDK.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 11/25/16/12:26 PM
+ *
+ */
+
+namespace GoalAPI\SDKBundle;
+
+class GoalAPISDK
+{
+}
---
+++ Tests/GoalAPISDKTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 11/25/16/12:27 PM
+ *
+ */
+
+namespace GoalAPI\SDKBundle\Tests;
+
+class GoalAPISDKTest extends \PHPUnit_Framework_TestCase
+{
+}
