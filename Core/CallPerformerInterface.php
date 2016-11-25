8fcabd4a9e698c5c9b64e3cb904c6d1a1d036df4
Murat Erkenov <murat@11bits.net>
2016-11-25 15:16:29 +0300

    SDK calls is done using CallPerformers

---
+++ Core/CallPerformerInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 11/25/16/1:44 PM
+ *
+ */
+
+namespace GoalAPI\SDKBundle\Core;
+
+interface CallPerformerInterface
+{
+    /**
+     * This method contains behavior which should be implemented by this Call Performer
+     *
+     * @param array $arguments
+     * @return mixed
+     */
+    function performCall(array $arguments);
+}
