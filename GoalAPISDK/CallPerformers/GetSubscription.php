f8bec31bf0de65764ffa91e1062cded19a4179e7
Murat Erkenov <murat@11bits.net>
2016-11-29 16:51:09 +0300

    GetSubscription API call is done using APIClient

--- GoalAPISDK/CallPerformers/GetSubscription.php
+++ GoalAPISDK/CallPerformers/GetSubscription.php
@@ -9,67 +9,35 @@ namespace GoalAPI\SDKBundle\GoalAPISDK\CallPerformers;
 
 use GoalAPI\SDKBundle\GoalAPISDK\CallPerformer;
 use GoalAPI\SDKBundle\Model;
-use GuzzleHttp\Client;
 
 class GetSubscription extends CallPerformer
 {
-    /**
-     * @var string
-     */
-    private $apikey;
-
-    /**
-     * GetSubscription constructor.
-     * @param $apikey
-     */
-    public function __construct($apikey)
-    {
-        $this->apikey = $apikey;
-    }
-
     public function loadDataFromProvider()
     {
-        $guzzleClient = new Client(
-            [
-                'base_uri' => 'http://api.goalapi.com/v1/',
-                'headers' => [
-                    'X-AUTH-APIKEY' => $this->apikey,
-                ],
-            ]
-        );
-        $response = $guzzleClient->get('');
-        $body = json_decode($response->getBody(), true);
+        $apiClient = $this->getApiClient();
+        $response = $apiClient->makeAPICall('');
 
-        return $body;
+        return $response->getData();
     }
-
 
     public function deserializeData($data)
     {
         $subscription = new Model\Subscription();
-        $subscription->setStatus($data['status']);
-        $subscription->setExpirationTime(new \DateTime($data['expirationTime']['date_time']));
+        $subscription->setStatus($data->status);
+        $subscription->setExpirationTime(new \DateTime($data->expirationTime->date_time));
 
-        if (isset($data['allowedTournaments'])) {
+        if (isset($data->allowedTournaments)) {
             $tournamentObjects = [];
-            foreach ($data['allowedTournaments'] as $tournamentItem) {
+            foreach ($data->allowedTournaments as $tournamentItem) {
                 $tournamentObject = new Model\Tournament();
-                $tournamentObject->setId($tournamentItem['id']);
-                $tournamentObject->setName($tournamentItem['name']);
+                $tournamentObject->setId($tournamentItem->id);
+                $tournamentObject->setName($tournamentItem->name);
                 $tournamentObjects[] = $tournamentObject;
             }
             $subscription->setTournaments($tournamentObjects);
         }
 
         return $subscription;
-    }
-
-    /**
-     * @param string $apikey
-     */
-    public function setApikey($apikey)
-    {
-        $this->apikey = $apikey;
     }
 
     public function mustRefresh()
--- Tests/GoalAPISDK/CallPerformers/GetSubscriptionTest.php
+++ Tests/GoalAPISDK/CallPerformers/GetSubscriptionTest.php
@@ -8,7 +8,6 @@
 namespace GoalAPI\SDKBundle\Tests\GoalAPISDK\CallPerformers;
 
 use GoalAPI\SDKBundle\GoalAPISDK;
-use GoalAPI\SDKBundle\GoalAPISDK\CallPerformers\GetSubscription;
 use GoalAPI\SDKBundle\Model;
 
 class GetSubscriptionTest extends \PHPUnit_Framework_TestCase
@@ -16,20 +15,82 @@ class GetSubscriptionTest extends \PHPUnit_Framework_TestCase
 
     function testGetSubscriptionCallPerformer()
     {
-        $apikey = '*';
-        $callPerformer = new GetSubscription($apikey);
+        $dataObject = $this->createDataObject();
+        $callPerformer = $this->createCallPerformer($dataObject);
+
         /** @var Model\Subscription $subscription */
         $subscription = $callPerformer->performCall([]);
         $this->assertInstanceOf(Model\Subscription::class, $subscription);
+        $this->assertEquals(new \DateTime($dataObject->expirationTime->date_time), $subscription->getExpirationTime());
+
+    }
+
+    /**
+     * @return object
+     */
+    private function createDataObject()
+    {
+        $dataObject = (object)[
+            'status' => 'ok',
+            'allowedTournaments' => [
+                (object)[
+                    'id' => 'rus_pl',
+                    'name' => 'Russia - Premier League',
+                ],
+                (object)[
+                    'id' => 'eng_pl',
+                    'name' => 'England - Premier League',
+                ],
+            ],
+            'expirationTime' => (object)[
+                'date_time' => '2017-08-01T22:49:08+00:00',
+                'timestamp' => 1501627748,
+            ],
+        ];
+
+        return $dataObject;
+    }
+
+    /**
+     * @param $dataObject
+     * @return GoalAPISDK\CallPerformers\GetSubscription
+     */
+    private function createCallPerformer($dataObject)
+    {
+        $apiClient = $this->createPartialMock(
+            GoalAPISDK\APIClient\Guzzle\Client::class,
+            [
+                'makeAPICall',
+            ]
+        );
+        $response = $this->createPartialMock(
+            GoalAPISDK\APIClient\Guzzle\Response::class,
+            [
+                'getData',
+            ]
+        );
+        $response->method('getData')->willReturn(
+            $dataObject
+        );
+        $apiClient->method('makeAPICall')->willReturn(
+            $response
+        );
+
+        $callPerformer = new GoalAPISDK\CallPerformers\GetSubscription();
+        $callPerformer->setApiClient($apiClient);
+
+        return $callPerformer;
     }
 
     function testGetSubscriptionSDKMethod()
     {
+        $dataObject = $this->createDataObject();
+        $callPerformer = $this->createCallPerformer($dataObject);
         $sdk = new GoalAPISDK();
-        $sdk->addCallPerformer('getSubscription', new GetSubscription('*'));
+        $sdk->addCallPerformer('getSubscription', $callPerformer);
 
-        /** @var Model\Subscription $subscription */
         $subscription = $sdk->getSubscription();
         $this->assertInstanceOf(Model\Subscription::class, $subscription);
+        $this->assertEquals(new \DateTime($dataObject->expirationTime->date_time), $subscription->getExpirationTime());
     }
 }
