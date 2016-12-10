8dec3e2631c458fd6168438233e3f40b695d8067
Murat Erkenov <murat@11bits.net>
2016-12-10 23:20:57 +0300

    GetStages call performer with test

---
+++ GoalAPISDK/CallPerformers/GetStages.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 12/9/16/10:02 AM
+ *
+ */
+
+namespace GoalAPI\SDKBundle\GoalAPISDK\CallPerformers;
+
+use GoalAPI\SDKBundle\GoalAPISDK\CallPerformer;
+use GoalAPI\SDKBundle\Model;
+
+class GetStages extends CallPerformer
+{
+    /**
+     * @param Model\Tournament $tournament
+     * @return string
+     */
+    public function loadDataFromProvider(Model\Tournament $tournament = null, Model\Season $season = null)
+    {
+        $response = $this->apiClient->makeAPICall(
+            '/tournaments/'.$tournament->getId().'/seasons/'.$season->getId().'/stages/'
+        );
+
+        return $response->getBody();
+    }
+
+    /**
+     * @param $data
+     * @return Model\Season[]
+     */
+    public function deserializeData($data)
+    {
+        /** @var Model\Season[] $seasons */
+        $seasons = $this->serializer->deserialize($data, Model\Stage::class.'[]', 'json');
+
+        return $seasons;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function mustRefresh()
+    {
+        return true;
+    }
+}
