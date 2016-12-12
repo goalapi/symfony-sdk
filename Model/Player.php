2fc85f23f42fc40e51cc40b039088916e7371e40
Murat Erkenov <murat@11bits.net>
2016-12-12 17:47:40 +0300

    Player object done

---
+++ Model/Player.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 12/11/16/9:29 AM
+ *
+ */
+
+namespace GoalAPI\SDKBundle\Model;
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class Player
+{
+    /**
+     * @var String
+     */
+    private $id;
+
+    /**
+     * @var String
+     */
+    private $name;
+
+    /**
+     * @var ParameterBag
+     */
+    private $names;
+
+    /**
+     * @var ParameterBag
+     */
+    private $bio;
+
+    /**
+     * @var String
+     */
+    private $position;
+
+    /**
+     * @var Territory
+     */
+    private $country;
+
+    /**
+     * @return String
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param String $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return String
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param String $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return ParameterBag
+     */
+    public function getNames()
+    {
+        return $this->names;
+    }
+
+    /**
+     * @param ParameterBag $names
+     */
+    public function setNames($names)
+    {
+        $this->names = $names;
+    }
+
+    /**
+     * @return ParameterBag
+     */
+    public function getBio()
+    {
+        return $this->bio;
+    }
+
+    /**
+     * @param ParameterBag $bio
+     */
+    public function setBio($bio)
+    {
+        $this->bio = $bio;
+    }
+
+    /**
+     * @return String
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param String $position
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @return Territory
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param Territory $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+}
