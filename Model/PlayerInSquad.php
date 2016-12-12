989298c80f9763c180ac503480fd5fa20e619e4c
Murat Erkenov <murat@11bits.net>
2016-12-12 19:36:56 +0300

    PlayerInSquad object

---
+++ Model/PlayerInSquad.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 12/12/16/6:35 PM
+ *
+ */
+
+namespace GoalAPI\SDKBundle\Model;
+
+class PlayerInSquad
+{
+    /**
+     * @var Player
+     */
+    private $player;
+
+    /**
+     * @var String
+     */
+    private $number;
+
+    /**
+     * @var String
+     */
+    private $position;
+
+    /**
+     * @return String
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param String $number
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return Player
+     */
+    public function getPlayer()
+    {
+        return $this->player;
+    }
+
+    /**
+     * @param Player $player
+     */
+    public function setPlayer($player)
+    {
+        $this->player = $player;
+    }
+
+    /**
+     * @return String
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param String $position
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+}
