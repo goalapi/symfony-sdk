69283055ceb6cbf28beb4c2157d2503e9c41456b
Murat Erkenov <murat@11bits.net>
2017-05-05 18:54:21 +0300

    Not found test

---
+++ Tests/integration/TestCases/ProviderTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 4/10/17/7:48 PM
+ *
+ */
+
+namespace GoalAPI\SDKBundle\Tests\integration\TestCases;
+
+use GoalAPI\SDKBundle;
+use GoalAPI\SDKBundle\Tests\integration\SDKTestCase;
+
+class ProviderTest extends SDKTestCase
+{
+
+    public function test404()
+    {
+
+        /** @var SDKBundle\GoalAPISDK $theSDK */
+        $theSDK = $this->getSDKInstance();
+
+        /** @var SDKBundle\Model\Tournament $tournament */
+        $tournament = new SDKBundle\Model\Tournament();
+        $tournament->setId('rus_pl');
+
+        $season = new SDKBundle\Model\Season();
+        $season->setId('rus_pl.20162017');
+
+        $stageId = 'rus_pl.20162017.zopa';
+        $this->expectException(SDKBundle\SDK\Exception\SDKException::class);
+        $theSDK->getStage($tournament, $season, $stageId);
+    }
+}
