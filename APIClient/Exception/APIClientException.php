30930fde02a4beee3399888724b59573758a7175
Murat Erkenov <murat@11bits.net>
2017-05-05 18:16:52 +0300

    APIClientException introduced

---
+++ APIClient/Exception/APIClientException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Murat Erkenov
+ * Date/Time: 5/5/17/5:10 PM
+ *
+ */
+
+namespace GoalAPI\SDKBundle\APIClient\Exception;
+
+use GoalAPI\SDKBundle\SDK\Exception\SDKException;
+
+class APIClientException extends SDKException
+{
+
+}
